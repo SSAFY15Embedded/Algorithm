830fc45cd23ff13a1381ed93b2969dd36a68dd43
yesosj <89458649+yesosj@users.noreply.github.com>
2026-02-09 19:18:55 +0900

    Update BOJ_1316_오승준.cpp

--- Week02_String/BOJ_1316_오승준.cpp
+++ Week02_String/BOJ_1316_오승준.cpp
@@ -12,28 +12,27 @@ int main() {
         cin >> s;
         for (int j = 0; j < s.length(); j++) {
             char temp = s[j];
-            if (visited[temp - 0]) {
-                ok = true;
-                break;
+            if (visited[temp - 0]) { // 현재의 단어에 재방문했다면 
+                ok = true; // cnt를 세지 않도록.
+                break; // 나가기
             } // 그룹단어 존재 시 나가기.
-            visited[temp - 0] = 1;
-            for (int k = j + 1; k < s.length(); k++) {
+            visited[temp - 0] = 1; // 방문한 글자 방문표시
+            for (int k = j + 1; k < s.length(); k++) { // 다음 글자 접근
 
                 if (temp != s[k]) { //그전값과 다르다면 나가기.
-                    j = k; //
-                 
+                    j = k; // 다음 글자로 이동해서 탐색.
                     break;
 
                 }
-                j = k;
+                j = k;// 다음 글자로 이동해서 탐색.
 
             }
 
         }
-        if (!ok) {
-            cnt++;
+        if (!ok) { //재방문이 되지않고 종료된 경우.
+            cnt++; 
         }
-        ok = false;
+        ok = false; // 초기화
 
     }
 
