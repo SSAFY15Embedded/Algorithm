5cbcf5330fff7242fff8633fdd460843af5e364e
joohan31 <160471895+joohan31@users.noreply.github.com>
2026-02-23 19:03:12 +0900

    Improve card queue logic and add comments
    Refactor queue implementation for card game logic.

--- Week03_Queue/BOJ_2164_윤주한.cpp
+++ Week03_Queue/BOJ_2164_윤주한.cpp
@@ -4,27 +4,32 @@ using namespace std;
 
 int input;
 int arr[MAX];
-int front = 0;
-int rear;
+int front = 0; //카드 덱의 맨 위
+int rear; //카드 덱의 맨 아래
 int ans;
 
 int main() {
+    //1. 초기화
     cin >> input;
     for(int i = 0; i < input; i++){
         arr[i] = i + 1;
     }
     rear = input - 1;
 
+    //2. 실행
     int cnt = 1;
     while(1){
+        //2-1. 확인: 카드가 한장이면(front와 rear가 같으면) 정답
         if(front == rear){
             ans = arr[front];
             break;
         }
-        
+
+        //2-1. 홀수 번째는 맨위 카드를 버린다(front를 한칸 옮김으로서 수행)
         if(cnt % 2 == 1){
             front += 1;
         }
+        //2-2. 짝수 번째는 맨위 카드를 아래로 옮긴다(rear의 한칸 뒤에 front에 해당하는 카드를 복사하고 front를 한칸 옮김)
         else if(cnt % 2 == 0){
             rear += 1;
             if(rear == MAX){
@@ -34,6 +39,7 @@ int main() {
             front += 1;
         }
 
+        //**front나 rear나 하나씩 더할 때 마다 MAX인지 확인하여 배열의 맨 앞으로 위치를 바꾸는 작업이 필요
         if(front == MAX){
             front = 0;
         }
